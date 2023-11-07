f36093abab6b0387aa7470aeec0188cdc7652046
Caroline <wanguic49@gmail.com>
2023-11-07 12:09:09 +0300

    few changes made

--- 3-modules.js
+++ 3-modules.js
@@ -10,21 +10,3 @@ sayHi(names.peter);
 
 console.log(data);
 
-//OS Module
-const { log } = require('console');
-const os = require('os');
-
-//info abt current user
-const user = os.userInfo();
-console.log(user);
-
-//returns the systems uptime  in seconds
-console.log(`The system UpTime is ${os.uptime()} seconds`);
-
-const currentOS = {
-    name: os.type(),
-    release: os.release(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem()
-}
-console.log(currentOS);
