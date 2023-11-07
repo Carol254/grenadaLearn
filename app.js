0098d481491df96909b95c99178ab720b46be528
Caroline <wanguic49@gmail.com>
2023-11-07 12:44:47 +0300

    reading file content

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const first  = readFileSync('./content/first.txt','utf8');
 const second = readFileSync('./content/second.txt','utf8');
 
 console.log(first,second);
+
+//creating and writing content in a file
+writeFileSync('./content/result-sync.txt',`Here is the result : ${first}, ${second}`
+,{flag: 'a'}
+);
